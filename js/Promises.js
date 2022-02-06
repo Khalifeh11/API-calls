00692e063821f05ce78fb6de3b92aa02bf20d46c
Karim <abdelkarim.kh95@gmail.com>
2022-02-06 15:58:36 +0200

    styles + flag icons

--- js/Promises.js
+++ js/Promises.js
@@ -11,6 +11,8 @@
 // The api for getting the age is: https://api.agify.io/?name=joe
 // The api for getting the nationality: https://api.nationalize.io/?name=mohamad
 // The api for getting the dog images: https://dog.ceo/api/breeds/image/random
+
+
 const img = document.querySelector('img');
 async function getRandomImg ()  {
     try{
@@ -20,58 +22,85 @@ async function getRandomImg ()  {
         return data
     }catch(error){
         console.log("ERROR: ", error)
-        throw "ERROR2"
+        throw "ERROR"
     }
     
 }
 getRandomImg();
 
 
-var nameInput = document.getElementById('name');
-var gender = document.querySelector(".gender");
-var age = document.querySelector(".age");
-var nationality = document.querySelector(".nationality");
+const nameInput = document.getElementById('name-input');
+const gender = document.querySelector(".gender");
+const age = document.querySelector(".age");
+const nationality = document.querySelector(".nationality");
+
 
 
 document.querySelector('.name-form').addEventListener('submit', function (e) {
     //prevent the normal submission of the form
     e.preventDefault()
     getAge();
-    getNationality();
+    addNationality();
     getGender();
     return nameInput.value;    
 });
 
 async function getGender() {
+    try{
     const response = await fetch(`https://api.genderize.io?name=${nameInput.value}`);
     const data = await response.json();
     gender.textContent = "gender " + `${data.gender}` 
     return data
+    }catch(error){
+        console.log("ERROR: ", error)
+        throw "ERROR"
+    }
+    
 }
 
 
 
 async function getAge() {
-    const response = await fetch(`https://api.agify.io/?name=${nameInput.value}`);
-    const data = await response.json();
-    age.textContent = "age " + `${data.age}`
-    return data
-}
-
-
-async function getNationality() {
-    const response = await fetch(`https://api.nationalize.io/?name=${nameInput.value}`);
-    const data = await response.json();
-    // data.forEach(element => {
-    //     console.log(data.country[element]["country_id"])
-    // });
-    console.log(data.country);
-    for (let i = 0; i < data.country.length; i++){
-        const textNode = document.createTextNode(`${ " " + data.country[i]["country_id"] + " "}`);
-        nationality.appendChild(textNode);
+    try{
+        const response = await fetch(`https://api.agify.io/?name=${nameInput.value}`);
+        const data = await response.json();
+        age.textContent = "age " + `${data.age}`
+        return data
     }
-    return data
+    catch(error){
+        console.log("ERROR: ", error)
+        throw "ERROR2"
+    }
+   
 }
+
+
+
+const flagDiv = document.querySelector(".flag-div")
+async function addNationality() {
+    try{
+        const response = await fetch(`https://api.nationalize.io/?name=${nameInput.value}`);
+        const data = await response.json();
+        while(flagDiv.firstChild){
+            flagDiv.removeChild(flagDiv.firstChild)
+        }
+        for (let i = 0; i < data.country.length; i++){
+            const flagIcon = new Image(40,40);
+            flagIcon.classList.add("flag-icon")
+            flagIcon.src = `images/flags/${data.country[i]["country_id"]}.svg`
+            flagDiv.appendChild(flagIcon);
+        } 
+        return data
+    }catch(error){
+        console.log("ERROR: ", error)
+        throw "ERROR"
+    }
+   
+}
+
+
+
+
 
 
 
